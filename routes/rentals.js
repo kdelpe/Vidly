8462df7f8332edee8d3091cf07d1e9fd47c3cc0e
Kervens Delpe <kdelpe@gmail.com>
2023-07-05 22:31:22 -0400

    fawn integration for db transactions

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,6 +2,7 @@ const { Rental, validate } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const mongoose = require("mongoose");
+const Fawn = require("fawn");
 const express = require("express");
 const router = express.Router();
 
@@ -38,12 +39,23 @@ router.post("/", async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate,
     },
   });
-  rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
+  try {
+    new Fawn.Task()
+      .save("rentals", rental)
+      .update(
+        "movies",
+        { _id: movie._id },
+        {
+          $inc: { numberInStock: -1 },
+        }
+      )
+      .run();
 
-  res.send(rental);
+    res.send(rental);
+  } catch (ex) {
+    res.status(500).send("Something failed.");
+  }
 });
 
 // GET a rental by id
